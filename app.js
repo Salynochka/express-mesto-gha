8ff3428fdbe58c0a859ca160811bd689086c3b9a
Alena Salynskaia <al_20.07@mail.ru>
2023-08-27 21:35:45 +0500

    deleted

--- app.js
+++ app.js
@@ -24,21 +24,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.post('/signin', validateUser, login);
 app.post('/signup', validateUser, createUser);
 
-app.use(auth, () => {});
-
-app.use('/cards', (req, res) => {
+app.use(auth, (req, res) => {
   if (!auth) {
     res.status(401).send({ message: 'Необходимо авторизоваться' });
   }
-  return routerCards;
 });
 
-app.use('/users', (req, res) => {
-  if (!auth) {
-    res.status(401).send({ message: 'Необходимо авторизоваться' });
-  }
-  return routerUsers;
-});
+app.use('/cards', validateCard, routerCards);
+
+app.use('/users', validateUser, routerUsers);
 
 app.use('*', (req, res) => res.status(404).send({ message: 'Неправильный путь' }));
 
