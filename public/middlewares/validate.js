0e3cd944229a8924ee6b4ab1feb8e64b2fd925f7
Alena Salynskaia <al_20.07@mail.ru>
2023-08-27 21:57:22 +0500

    updated validation of user

--- public/middlewares/validate.js
+++ public/middlewares/validate.js
@@ -2,9 +2,9 @@ const { celebrate, Joi } = require('../../node_modules/celebrate');
 
 module.exports.validateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2),
-    avatar: Joi.string().pattern(/https?:\/\/\w{3}?\.\S[0,]*/),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2),
+    avatar: Joi.string().pattern(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(2),
   }),
