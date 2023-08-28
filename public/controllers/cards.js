64a12041c3a9bd42769396700e0f9547e303eead
Alena Salynskaia <al_20.07@mail.ru>
2023-08-29 01:27:57 +0500

    one more fix

--- public/controllers/cards.js
+++ public/controllers/cards.js
@@ -71,7 +71,7 @@ module.exports.addLike = (req, res) => {
 module.exports.deleteLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.params.userId } }, // убрать _id из массива
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
     .then((card) => {
