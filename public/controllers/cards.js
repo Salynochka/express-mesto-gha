9791a81906ed8d48514a2a7a1b9d4a1748c34cdd
Alena Salynskaia <al_20.07@mail.ru>
2023-08-31 14:28:32 +0500

    one more attempt

--- public/controllers/cards.js
+++ public/controllers/cards.js
@@ -12,15 +12,9 @@ module.exports.getCards = (req, res, next) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const { owner } = req.params;
 
-  Card.create({ name, link, owner })
-    .then((card) => res.send({
-      name: card.name,
-      link: card.link,
-      _id: card._id,
-      owner: req.params.userId, // ИЗМЕНЕНО
-    }))
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(INCORRECT_DATA).send({ message: 'Произошла ошибка' });
